97ad073049844a78ab147d36697543e21d95e44b
ryangustav <ryang6329@gmail.com>
2026-02-20 19:44:52 -0300

    fix: mongo uri error

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,15 @@ module.exports = {
   apps: [
     {
       name: 'master-cheat',
-      script: 'dist/main.js',
+      script: 'start.js',
       cwd: path.resolve(__dirname, 'backend'),
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '256M',
-      interpreter_args: '-r dotenv/config',
       env: {
         NODE_ENV: 'production',
         PORT: 3001,
-        DOTENV_CONFIG_PATH: path.resolve(__dirname, 'backend', '.env'),
       },
     },
   ],
